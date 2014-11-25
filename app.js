98d7fb56d973c62cecc37672d2b2217b6e2e194c
albertcui <albertqcui+github@gmail.com>
2014-11-25 02:35:56 +0000

    dump json to browser

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 var solr = require('solr-client'),
-    client = solr.createClient('clio-lab.cul.columbia.edu', 8983, null, '/solr-4.2.1/spectrum_lab')
+    client = solr.createClient('clio-lab.cul.columbia.edu', 8983, null, '/solr-4.2.1/spectrum_lab'),
+    express = require('express'),
+    app = express()
 
+app.route('/').get(function(req, res){
+    client.search('q=potato', function(err, obj){
+        res.send(obj.response.docs)
+    })
+})
+
+
+app.listen(5000)
+/*                 
 var request = client.search('q=hello', function(err, obj){
     obj.response.docs.forEach(function(doc){
         console.log(doc)
@@ -10,3 +21,5 @@ var request = client.search('q=hello', function(err, obj){
 request.setTimeout(200, function(){
     console.log('search timeout');
 });
+
+*/
